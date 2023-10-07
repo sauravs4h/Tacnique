dec89db2876aa6f83d762c1ff5dd30c336bbbb31
sauravs4h <sauravs4h@gmail.com>
2023-10-07 09:25:01 +0530

    task route done

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -3,7 +3,8 @@ const {Taskmodel}=require("../models/taskmodel")
 
 const task=express.Router();
 
-task.get("/alltask",async(req,res)=>{
+// Route for all task 
+task.get("/tasks",async(req,res)=>{
     const {userid}=req.body;
     try {
         let tasks=await Taskmodel.find({userid});
@@ -14,8 +15,70 @@ task.get("/alltask",async(req,res)=>{
     }
 })
 
+// Route for one specific task
+task.get("/tasks/:id",async(req,res)=>{
+    const {userid}=req.body;
+    const taskid=req.params.id
+    try {
+        let task=await Taskmodel.findOne({_id:taskid});
+        res.status(201).json({tasks:task,status:"success"});
+    } catch (error) {
+        res.status(401).json({mag:"something went wrong",status:"failed"});
+        
+    }
+});
 
 
+// Route for add new task
+
+task.post("/tasks",async(req,res)=>{
+    const payload=req.body;
+    try {
+        let newtask=new Taskmodel(payload);
+        await newtask.save();
+        res.status(201).json({msg:"task add successfully added",status:"success"});
+    } catch (error) {
+        res.status(401).json({mag:"something went wrong",status:"failed"});
+        
+    }
+})
+
+// Update a specific task
+
+task.put("/tasks/:id",async(req,res)=>{
+    const payload=req.body;
+    const taskid=req.params.id;
+    try {
+        let task=await Taskmodel.findOne({_id:taskid});
+        if(task){
+            await Taskmodel.findByIdAndUpdate({_id:taskid},payload);
+            res.status(201).json({msg:"Task added successfully",status:"success"});
+        }else{
+            res.status(401).json({mag:"Task is not available",status:"failed"});
+        } 
+    } catch (error) {
+        res.status(401).json({mag:"something went wrong",status:"failed"}); 
+    }
+})
+
+// Delete one specific task
+
+
+
+task.delete("/tasks/:id",async(req,res)=>{
+    const taskid=req.params.id;
+    try {
+        let task=await Taskmodel.findOne({_id:taskid});
+        if(task){
+            await Taskmodel.findByIdAndDelete({_id:taskid});
+            res.status(201).json({msg:"task delete successfully",status:"success"});
+        }else{
+            res.status(401).json({mag:"Task is not available",status:"failed"});
+        } 
+    } catch (error) {
+        res.status(401).json({mag:"something went wrong",status:"failed"}); 
+    }
+})
 
 
 module.exports={task}
